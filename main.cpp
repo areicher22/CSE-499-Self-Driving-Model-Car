1f16408a1986819509b31d221d6053912b18d6ad
Mohammad Tamzeed Moazzem <mmoazzem@buffalo.edu>
2022-05-12 13:38:55 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,38 +1,88 @@
-/*
-Written by: Austin Reichert, Mohammad Tamzeed Moazzem, Hasaan Saleh
-File purpose: To implement a PID controller which controls the steering angle of a psuedo-FSD car
-Modules/Subroutines: 
-    - main()
-    - pid()
+/* 
+ * File Name: main.cpp
+ * Developer: Mohammad Tamzeed Moazzem, Austin Reichert, Hasaan Saleh
+ * 
+ */
 
-Corresponds to: CSE 499 Final Project
-Inputs:
-Outputs:
-Constraints: 
-
-Sources/References: 
-    RM0432 Reference Manual
-    Dr.Winikus' CSE-321 Lectures
-    Arduino Open Source Libraries
-    University of Michigan Control Tutorials: https://ctms.engin.umich.edu/CTMS/index.php?example=Introduction&section=ControlPID
-    thingyverse chassis: https://www.thingiverse.com/thing:4892947/files
-
-*/
-
+#include "LiDAR.h"
 #include "mbed.h"
+#include "ultrasonic.h"
+#include <cstdio>
 
-float pid(float theta); //function that handles the PID controller calculations
-                        //takes in lidar and current servo position as args and outputs new steering angle
+#define MIN 1000
+#define MID 1500
+#define MAX 2000
+#define STEP 30
+#define TIME 100 // Time delay between steps in milliseconds
 
+// void servo_move(int value);
 
+// LiDAR object initialization
+LiDAR lidar(PB_9, PB_8);
+
+// Ultrasonic sensor object initialization
+Ultrasonic left_sonar(PD_7, PD_6);
+
+// Ultrasonic sensor object initialization
+Ultrasonic right_sonar(PD_5, PD_4);
+
+// DigitalIn object initialization
+DigitalIn check(PG_0);
+
+// DigitalOut object initialization
+DigitalOut motor(PC_8);
+
+// PwmOut object initialization
+PwmOut servo(PD_15);
+
+float lider_dist = 0.0;
+float left_sonar_dist = 0.0;
+float right_sonar_dist = 0.0;
 
 // main() runs in its own thread in the OS
-int main()
-{
-    while (true) {
+int main() {
+  // LiDAR initialization to use
+  lidar.begin();
 
+  motor = 0;
+
+  // Servo period set to 20 millisecond
+  servo.period_ms(20);
+
+  while (true) {
+    if (check == 1) {
+      lider_dist = lidar.get_dist();
+      //printf("%f\n", lider_dist);
+
+      if (lider_dist < 20) {
+        left_sonar_dist = left_sonar.getCurrentDistance();
+        printf("%f\n", left_sonar_dist);
+        right_sonar_dist = right_sonar.getCurrentDistance();
+        printf("%f\n", right_sonar_dist);
+
+        servo = 1;
+
+        if (left_sonar_dist > right_sonar_dist) {
+        //   servo.pulsewidth_us(MAX);
+        //   wait_us(TIME * 200);
+          for (int i = MAX; i >= MIN; i -= STEP) {
+            servo.pulsewidth_us(i);
+            wait_us(TIME * 200);
+          }
+        } 
+        // else if(left_sonar_dist < right_sonar_dist) {
+
+        //   for (int i = MIN; i <= MAX; i += STEP) {
+        //     servo.pulsewidth_us(i);
+        //     wait_us(TIME * 200);
+        //   }
+        // }
+        servo = 0;
+      
+      } else {
+        motor = 1;
+      }
     }
+  }
+  return 0;
 }
-
-
-
